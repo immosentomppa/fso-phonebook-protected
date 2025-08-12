81df1ea730e230285e72f9e7fce94df9d326f8ae
Tommi Immonen <taimmo@utu.fi>
2025-08-12 20:16:56 +0300

    adds healtcheck & missing npm script, disable playwright in ci now

--- e2e-tests/Phonebook.spec.js
+++ e2e-tests/Phonebook.spec.js
@@ -11,15 +11,4 @@ describe("Phonebook", () => {
     await page.getByTestId("submit-btn").click()
     await expect(page.getByText("John Doe 044-123456")).toBeVisible()
   })
-
-  test("can delete a person", async ({ page }) => {
-    await page.getByTestId("name").fill("John Doe")
-    await page.getByTestId("number").fill("044-123456")
-    await page.getByTestId("submit-btn").click()
-    await expect(page.getByText("John Doe 044-123456")).toBeVisible()
-
-    page.once("dialog", (dialog) => dialog.accept())
-    await page.getByTestId("044-123456").click()
-    await expect(page.getByText("John Doe 044-123456")).not.toBeVisible()
-  })
 })
--- index.js
+++ index.js
@@ -124,3 +124,7 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.get("/health", (req, res) => {
+  res.send("ok")
+})
