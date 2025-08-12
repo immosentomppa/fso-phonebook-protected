1c510a084591f24fc8313103021f35212cd4e449
Tommi Immonen <taimmo@utu.fi>
2025-08-12 23:38:07 +0300

    test: maybe fixes tests

--- e2e-tests/utils.js
+++ e2e-tests/utils.js
@@ -3,7 +3,7 @@ export async function waitForPerson(page, name, timeout = 3000) {
   while (Date.now() - start < timeout) {
     const response = await page.request.get("/api/persons")
     const persons = await response.json()
-    const person = persons.find((p) => p.name === name)
+    const person = persons.find((p) => p.name.includes(name.trim()))
     if (person) return person
     await page.waitForTimeout(200)
   }
